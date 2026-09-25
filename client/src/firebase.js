37dbd5c8ad087708ae94d63f195f14718cab5a74
unknown <namansharma3194@gmail.com>
2026-09-25 18:33:16 +0530

    Update Firebase web app credentials and storage bucket

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,8 +5,9 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN || "taskmanager-557d7-57874.firebaseapp.com",
   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID || "taskmanager-557d7-57874",
-  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET || "taskmanager-557d7-57874.appspot.com",
-  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID || "1:707471138863:web:tasky"
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET || "taskmanager-557d7-57874.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID || "246625536592",
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID || "1:246625536592:web:e9a9dcd1617440cfdb9f55"
 };
 
 const app = initializeApp(firebaseConfig);
