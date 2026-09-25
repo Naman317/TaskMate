ecbbdfcc2b1b4960066427a5ce7369743d36fa37
unknown <namansharma3194@gmail.com>
2026-09-25 18:26:20 +0530

    Remove hardcoded Firebase API key string and load strictly from env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY || "REMOVED_FIREBASE_KEY",
-  authDomain: "taskmanager-557d7-57874.firebaseapp.com",
-  projectId: "taskmanager-557d7-57874",
-  storageBucket: "taskmanager-557d7-57874.appspot.com",
-  appId: "1:707471138863:web:tasky"
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN || "taskmanager-557d7-57874.firebaseapp.com",
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID || "taskmanager-557d7-57874",
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET || "taskmanager-557d7-57874.appspot.com",
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID || "1:707471138863:web:tasky"
 };
 
 const app = initializeApp(firebaseConfig);
